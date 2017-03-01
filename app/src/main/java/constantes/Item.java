b8c902820ace40ab06bd54b31080822f3e312131
MCristoni <matheusmgc@globo.com>
2017-03-01 00:20:44 -0300

    Mudanças básicas

---
+++ app/src/main/java/constantes/Item.java
@@ -0,0 +1,14 @@
+package constantes;
+
+/**
+ * Created by matheuscristoni on 28/02/17.
+ */
+
+public class Item
+{
+    public String tituloInfo;
+    public String mensagemInfo;
+    public String tituloItem;
+
+    public Item(){ }
+}
